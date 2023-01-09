20364cc4955dbb47622fc087d5bfd9745ec76183
spencer@primus <spencer.hallyburton@duke.edu>
2023-01-09 16:18:12 -0500

    Unset env vars before rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+set -e
+
 rm -rf log install build
+unset CMAKE_PREFIX_PATH
+unset COLCON_PREFIX_PATH
 colcon build --metas colcon_python.meta --cmake-args -DPYTHON_BINDING_VERSION=3.8
 cd install
 source setup.bash
